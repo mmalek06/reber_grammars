cd0181a5fe42414ae8339cc3132530d0b4976605
Marek Małek <mmalek06@gmail.com>
2023-02-04 10:12:18 +0100

    add grammar degeneration

--- grammar_degenerator.py
+++ grammar_degenerator.py
@@ -1,2 +1,14 @@
+import random
+
+
 with open('proper_reber.txt', 'r') as f:
-    pass
+    lines = [line.rstrip() for line in f]
+
+
+initial_length = len(lines)
+switch_end_char_size = int(.05 * initial_length)
+switch_end_char_list = random.choices(lines, k=switch_end_char_size)
+lines = list(set(lines) - set(switch_end_char_list))
+switch_start_char_size = int(.05 * initial_length)
+switch_start_char_list = random.choices(lines, k=switch_start_char_size)
+lines = list(set(lines) - set(switch_start_char_list))
