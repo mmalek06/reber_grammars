dd224a7777775d38d8f5e847b317fae90e816b26
Marek Małek <mmalek06@gmail.com>
2023-02-10 08:19:55 +0100

    simplification

--- base_grammars/grammar_generator.py
+++ base_grammars/grammar_generator.py
@@ -1,150 +1,39 @@
-import random
+import numpy as np
 
 
-class Rule:
-    def applies(self, string_so_far: str) -> bool:
-        pass
+default_reber_grammar = [
+    [("B", 1)],
+    [("T", 2), ("P", 3)],
+    [("S", 2), ("X", 4)],
+    [("T", 3), ("V", 5)],
+    [("X", 3), ("S", 6)],
+    [("P", 4), ("V", 6)],
+    [("E", None)]]
 
 
-class BRule(Rule):
-    LETTER = 'b'
+def generate_string() -> str:
+    state = 0
+    output = []
 
-    def applies(self, string_so_far: str) -> bool:
-        return string_so_far is None or len(string_so_far) == 0
+    while state is not None:
+        index = np.random.randint(len(default_reber_grammar[state]))
+        char, state = default_reber_grammar[state][index]
+        output.append(char)
 
-
-class TRule(Rule):
-    LETTER = 't'
-
-    def applies(self, string_so_far: str) -> bool:
-        match string_so_far:
-            case [letter] if letter == BRule.LETTER:
-                return True
-            case [*_, BRule.LETTER, PRule.LETTER] | \
-                 [*_, PRule.LETTER, TRule.LETTER] | \
-                 [*_, TRule.LETTER, TRule.LETTER] | \
-                 [*_, XRule.LETTER, XRule.LETTER] | \
-                 [*_, XRule.LETTER, TRule.LETTER] | \
-                 [*_, PRule.LETTER, XRule.LETTER]:
-                return True
-            case _:
-                return False
+    return ''.join(output)
 
 
-class PRule(Rule):
-    LETTER = 'p'
-
-    def applies(self, string_so_far: str) -> bool:
-        match string_so_far:
-            case [BRule.LETTER] | \
-                 [*_, TRule.LETTER, VRule.LETTER] | \
-                 [*_, XRule.LETTER, VRule.LETTER] | \
-                 [*_, PRule.LETTER, VRule.LETTER]:
-                return True
-            case _:
-                return False
-
-
-class VRule(Rule):
-    LETTER = 'v'
-
-    def applies(self, string_so_far: str) -> bool:
-        match string_so_far:
-            case [BRule.LETTER, PRule.LETTER] | \
-                 [*_, PRule.LETTER, TRule.LETTER] | \
-                 [*_, TRule.LETTER, TRule.LETTER] | \
-                 [*_, XRule.LETTER, TRule.LETTER] | \
-                 [*_, XRule.LETTER, XRule.LETTER] | \
-                 [*_, PRule.LETTER, VRule.LETTER] | \
-                 [*_, TRule.LETTER, VRule.LETTER] | \
-                 [*_, XRule.LETTER, VRule.LETTER] | \
-                 [*_, PRule.LETTER, XRule.LETTER]:
-                return True
-            case _:
-                return False
-
-
-class XRule(Rule):
-    LETTER = 'x'
-
-    def applies(self, string_so_far: str) -> bool:
-        match string_so_far:
-            case [BRule.LETTER, TRule.LETTER] | \
-                 [*_, TRule.LETTER, SRule.LETTER] | \
-                 [*_, SRule.LETTER, SRule.LETTER] | \
-                 [*_, TRule.LETTER, XRule.LETTER] | \
-                 [*_, SRule.LETTER, XRule.LETTER] | \
-                 [*_, VRule.LETTER, PRule.LETTER]:
-                return True
-            case _:
-                return False
-
-
-class SRule(Rule):
-    LETTER = 's'
-
-    def applies(self, string_so_far: str) -> bool:
-        match string_so_far:
-            case [BRule.LETTER, TRule.LETTER] | \
-                 [*_, TRule.LETTER, SRule.LETTER] | \
-                 [*_, SRule.LETTER, SRule.LETTER] | \
-                 [*_, TRule.LETTER, XRule.LETTER] | \
-                 [*_, SRule.LETTER, XRule.LETTER] | \
-                 [*_, VRule.LETTER, PRule.LETTER]:
-                return True
-            case _:
-                return False
-
-
-class ERule(Rule):
-    LETTER = 'e'
-
-    def applies(self, string_so_far: str) -> bool:
-        match string_so_far:
-            case [*_, XRule.LETTER, SRule.LETTER] | \
-                 [*_, PRule.LETTER, SRule.LETTER] | \
-                 [*_, VRule.LETTER, VRule.LETTER]:
-                return True
-            case _:
-                return False
-
-
-rules = [BRule(), TRule(), PRule(), VRule(), XRule(), SRule(), ERule()]
-
-
-def generate(rules: list[Rule]) -> set[str]:
+def generate() -> set[str]:
     lines = []
-    counter = 0
-    MAX = 500
 
     while len(set(lines)) < 2000:
-        letters = []
-
-        while True:
-            counter += 1
-            applicable_rules = list(filter(lambda rule: rule.applies(letters), rules))
-            chosen_rule = random.choice(applicable_rules)
-            next_letter = chosen_rule.LETTER
-
-            letters.append(next_letter)
-
-            if next_letter == ERule.LETTER:
-                break
-            if len(letters) >= MAX:
-                letters = []
-
-                break
-
-        if len(letters) > 0:
-            line = list(map(lambda char: char.upper(), letters))
-
-            lines.append(''.join(line))
+        lines.append(generate_string())
 
     return set(lines)
 
 
 if __name__ == '__main__':
-    lines = generate(rules)
+    lines = generate()
 
     with open('proper_reber.txt', 'w') as f:
         for line in lines:
--- embedded_grammars/embedded_grammar_generator.py
+++ embedded_grammars/embedded_grammar_generator.py
@@ -1,9 +1,9 @@
 import random
 
-from reber_grammars.base_grammars.grammar_generator import rules, generate
+from reber_grammars.base_grammars.grammar_generator import generate
 
 
-lines = list(generate(rules))
+lines = list(generate())
 
 for idx in range(len(lines)):
     beginning = random.choice(['BT', 'BP'])
@@ -11,7 +11,7 @@ for idx in range(len(lines)):
     line = f'{beginning}{lines[idx]}{ending}'
     lines[idx] = line
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     with open('proper_embedded_reber.txt', 'w') as f:
         for line in lines:
             f.write(f'{line}\n')
